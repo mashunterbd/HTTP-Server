bb6492a790170f723c40727100f691e4a3134343
M.A.S Hunter <108648096+mashunterbd@users.noreply.github.com>
2024-05-04 11:36:10 +0600

    Delete Delete.sh

--- Delete.sh
+++
@@ -1,6 +0,0 @@
-# Remove the server scripts from /usr/local/bin/
-sudo rm /usr/local/bin/server-control.sh /usr/local/bin/server.start
-
-# Remove the HTTP-Server directory
-cd ..
-rm -rf HTTP-Server
