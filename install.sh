367b59b7f7adbe6514f80da84449dfcb268a5ace
M.A.S Hunter <108648096+mashunterbd@users.noreply.github.com>
2024-05-04 11:35:46 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,13 +5,10 @@ git clone https://github.com/mashunterbd/HTTP-Server.git
 cd HTTP-Server
 
 # Make the server scripts executable
-chmod +x server.start server-control.sh
-
-# change file name server-control.sh to server
-mv server-control.sh server 
+chmod +x server
 
 # Move the server scripts to /usr/local/bin/ for easy access
-sudo mv server server.start /usr/local/bin/
+sudo mv server /usr/local/bin/
 
 # clear terminal 
 clear 
